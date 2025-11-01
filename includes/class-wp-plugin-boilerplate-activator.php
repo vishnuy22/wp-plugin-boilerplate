f5baef38ad2b49c59191ed0a2fc1a1a927629ef2
Vishnu Y <vishnu.y@grozeo.com>
2025-11-01 10:36:16 +0530

    Initial commit

---
+++ includes/class-wp-plugin-boilerplate-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://#
+ * @since      1.0.0
+ *
+ * @package    Wp_Plugin_Boilerplate
+ * @subpackage Wp_Plugin_Boilerplate/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Plugin_Boilerplate
+ * @subpackage Wp_Plugin_Boilerplate/includes
+ * @author     Vishnu Y <vishnu.y@outlook.com>
+ */
+class Wp_Plugin_Boilerplate_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-plugin-boilerplate-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://#
+ * @since      1.0.0
+ *
+ * @package    Wp_Plugin_Boilerplate
+ * @subpackage Wp_Plugin_Boilerplate/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Plugin_Boilerplate
+ * @subpackage Wp_Plugin_Boilerplate/includes
+ * @author     Vishnu Y <vishnu.y@outlook.com>
+ */
+class Wp_Plugin_Boilerplate_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
